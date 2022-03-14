d71ee4596aa0f9cf658cab018e6c029aa741b0df
decozzfx <m.f.r.ozil@gmail.com>
2022-03-14 13:00:50 +0700

    finishing reducer logic

--- redux/Shopping/shopping-reducer.js
+++ redux/Shopping/shopping-reducer.js
@@ -38,13 +38,28 @@ const shopReducer = (state = initial_state , action) => {
     // console.log(state)
     switch(actionTypes){
         case actionTypes.ADD_TO_CART :
-            return {}
+            // Get the item data from the products array base the id
+            const item = state.products.find(product => product.id === action.payload.id) 
+            // Check if Item is in cart already
+            const inCart = state.cart.find(item => item.id === action.payload.id ? true : false) 
+            return {
+                ...state,
+                cart : inCart ? state.cart.map(item => item.id === action.payload.id ? {...item, qty : item.qty + 1} : item) : [...state.cart, {...item, qty : 1}] // if incart(true) -> find item and change the qty to +1 and if not just replace data // and if its not incart(false) add item to the cart and add qty 1 for initial value of qty
+            }
         case actionTypes.REMOVE_FROM_CART :
-            return {}
+            return {
+                ...state,
+                cart : state.cart.filter(item => item !== action.payload.id)
+            }
         case actionTypes.ADJUST_QTY :
-            return {}
+            return {
+                ...state,
+                cart : state.cart.map(item => item.id === action.payload.id ? {...item, qty : action.payload.qty} : item)
+            }
         case actionTypes.LOAD_CURRENT_ITEM :
-            return {}
+            return {
+                ...state, currentItem : action.payload
+            }
         default :
             return state
     }
